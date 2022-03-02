ebd0d193c56586cb7a130e193b2983dfb3c39ec3
ermias19 <ermiasmulu19@gmail.com>
2022-03-02 11:13:01 +0300

    initiating week 2

--- week_2/evalutate_reverse.py
+++ week_2/evalutate_reverse.py
@@ -31,35 +31,3 @@ print(stacK.pop())
 
 
 
-# class Solution:
-#     def evalRPN(self, tokens: List[str]) -> int:
-#         stack = []
-#         ops = '+-/*'
-#         for i in range(0, len(tokens), 1):
-#             a = b = 0
-#             if tokens[i] not in ops:  
-#                 stack.append(int(tokens[i]))
-                
-#             elif tokens[i] == "+":
-#                 a = stack.pop()
-#                 b = stack.pop()
-#                 stack.append(int(b)+int(a))
-                
-#             elif tokens[i] == '-':
-#                 a = stack.pop()
-#                 b = stack.pop()
-#                 stack.append(int(b)-int(a))
-                
-#             elif tokens[i] == '*':
-#                 a = stack.pop()
-#                 b = stack.pop()
-#                 stack.append(int(b)*int(a))
-                
-#             elif tokens[i] == '/':
-#                 a = stack.pop()
-#                 b = stack.pop()
-#                 stack.append(int(int(b)/int(a)))
-                
-                
-        
-#         return stack.pop()
