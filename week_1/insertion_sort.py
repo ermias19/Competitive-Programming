c987b5834e456e147485ef17f874bbed97ce2e40
ermias19 <ermiasmulu19@gmail.com>
2022-03-02 08:38:10 +0300

    hacker rank

--- week_1/insertion_sort.py
+++ week_1/insertion_sort.py
@@ -1,10 +1,35 @@
-nums=[7,3,8,9,1,3,6]
+#question https://www.hackerrank.com/challenges/insertionsort1/problem
+#!/bin/python3
 
-for i in range(1,len(nums)):
-    counter=i
-    while nums[counter-1]<nums[counter] and counter>=0:
-       nums[counter], nums[counter-1]= nums[counter-1],nums[counter]
-       print(nums)
-       counter-=1
+import math
+import os
+import random
+import re
+import sys
 
- 
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    end = arr[-1]
+    idx = n-2
+
+    while (end < arr[idx]) and (idx >= 0):
+        arr[idx+1] = arr[idx]
+        print(*arr)
+        idx -= 1
+
+    arr[idx+1] = end
+    print(*arr)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
