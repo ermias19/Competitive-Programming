b1ce0795f7d3f3f33c7f34ad64383ce3a113c99b
ermias19 <ermiasmulu19@gmail.com>
2022-03-16 11:35:36 +0300

    adding power fo three

--- week3/power-of-three.py
+++ week3/power-of-three.py
@@ -1,9 +1,58 @@
-import math 
-def powerofthree(n):
-    if  n==1:
-        return True
-    else:
-        if n%3!=0 or n==0:
-            return False
-    return powerofthree(n//3) 
-print(powerofthree(9))
+def power_three(n):
+    if n==1 :
+        return True 
+    elif n%3!=0 or n==0:
+        return  False
+    return power_three(n//3)
+       
+
+        
+   
+
+
+
+
+print(power_three(9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import math 
+# def powerofthree(n):
+#     if  n==1:
+#         return True
+#     else:
+#         if n%3!=0 or n==0:
+#             return False
+#     return powerofthree(n//3) 
+# print(powerofthree(9))
